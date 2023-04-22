fd338d7188aafe97d231d4e1b476bbb6180cd8ba
mark-delchev <markdelchev@gmail.com>
2023-04-22 15:27:48 +0300

    new text generation model

--- text_generation.py
+++ text_generation.py
@@ -1,52 +1,47 @@
-from tokenization import tokenize_bulgarian_text
-from nltk import bigrams, trigrams
-from collections import Counter, defaultdict
+import nltk
 import random
+from nltk.tokenize import word_tokenize
+from collections import defaultdict
 
-with open('IvanVazov1.txt', 'r', encoding='utf-8') as f:
-    data = f.read()
+# Read the file and tokenize its contents into words
+with open('IvanVazov2.txt', 'r', encoding='utf-8') as f:
+    corpus = f.read()
+words = word_tokenize(corpus)
 
-# Tokenize text
-tokenized_sentences = tokenize_bulgarian_text(data)
-print(tokenized_sentences)
-
-# Create a placeholder for model
+# Create a placeholder for the language model
 model = defaultdict(lambda: defaultdict(lambda: 0))
 
-# Count frequency of co-occurance  
-for sentence in tokenized_sentences:
-    for w1, w2, w3 in trigrams(sentence, pad_right=True, pad_left=True):
-        print("Trigram:", (w1, w2, w3))
-        model[(w1, w2)][w3] += 1
- 
-# Transform the counts to probabilities
+# Count the frequency of co-occurrence of each word
+for i in range(len(words)-2):
+    w1 = words[i]
+    w2 = words[i+1]
+    w3 = words[i+2]
+    model[(w1, w2)][w3] += 1
+
+# Normalize the counts to probabilities
 for w1_w2 in model:
     total_count = float(sum(model[w1_w2].values()))
     for w3 in model[w1_w2]:
         model[w1_w2][w3] /= total_count
-print(model)
 
-
-# starting words
-text = ["беше"]
+# Generate text using the language model
+text = ['той', 'беше',]
 sentence_finished = False
- 
-print("Starting words:", text)
-
 while not sentence_finished:
-    # select a random probability threshold  
+    # Select a random probability threshold
     r = random.random()
-    accumulator = .0
+    accumulator = 0.0
+
+    # Choose the next word based on the probability distribution
     for word in model[tuple(text[-2:])].keys():
-        print(f"word: {word}, probability: {model[tuple(text[-2:])][word]}, accumulator: {accumulator}")
         accumulator += model[tuple(text[-2:])][word]
-        # select words that are above the probability threshold
         if accumulator >= r:
             text.append(word)
             break
 
+    # End the sentence if the model generates two consecutive None values
     if text[-2:] == [None, None]:
-        print("End of sentence.")
         sentence_finished = True
- 
-print (' '.join([t for t in text if t]))
+
+# Print the generated text
+print(' '.join(text))
--- tokenization.py
+++ tokenization.py
@@ -3,26 +3,24 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 import string
 from spacy.lang.bg.stop_words import STOP_WORDS as bg_stopwords
 
-
-
 def tokenize_bulgarian_text(text):
     extra_stopwords = {"—", "“", "„", "не", "та", "па"}
     bg_stopwords.update(extra_stopwords)
-    # Remove punctuation and lowercase all letters
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    text = text.lower()
     
     # Split the text into individual sentences
     sentences = sent_tokenize(text)
 
-    # Tokenize and filter each sentence
     filtered_tokens = []
     for sentence in sentences:
+        # Remove punctuation and lowercase all letters
+        sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+        sentence = sentence.lower()
+        
         # Split the sentence into individual words or tokens
         tokens = word_tokenize(sentence)
+        
         # Remove any stop words from the tokenized text
         filtered_sentence = [word for word in tokens if word not in bg_stopwords]
         filtered_tokens.append(filtered_sentence)
 
     return filtered_tokens
-
